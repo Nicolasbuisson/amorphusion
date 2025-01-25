3d677da5f2b82279a66207835c5c2ba76f3be7da
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2025-01-25 18:50:06 -0500

    finish footer + initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.css";
 import { TicketButton } from "@/components/ticketButton/ticketButton";
 
 export default function Home() {
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.h1}>Amorphusion</h1>
+      <main>
+        <h1>Amorphusion</h1>
         <h2>World Music that makes you dance</h2>
+        <h3>LIVE AT </h3>
+        <h3>
+          Cafe la Ligne Verte, 2531 Rue Ontario East Montréal, Montreal, QC,
+          Canada
+        </h3>
+        <h3>SATURDAY FEBRUARY 1ST | 8 PM</h3>
         <TicketButton />
       </main>
     </div>
---
+++ src/components/ticketButton/ticketButton.tsx
@@ -0,0 +1,12 @@
+import "./ticketButton.css";
+
+export const TicketButton = () => {
+  return (
+    <a
+      href="https://thepointofsale.com/tickets/amorphusionwonderland"
+      target="__blank"
+    >
+      <button className="ticket-button">BOOK NOW</button>
+    </a>
+  );
+};
